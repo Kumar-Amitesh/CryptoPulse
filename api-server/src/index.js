9efda459f59ba4559715609e430ddda6a476ee95
Kumar-Amitesh <amitesh04kumar@gmail.com>
2025-05-25 00:02:15 +0530

    Refactor project structure

--- api-server/src/index.js
+++ api-server/src/index.js
@@ -1,6 +1,20 @@
+import app from './app.js';
 import dotenv from 'dotenv';
-
+import connectDB from './src/config/db.js';
+import logger from './src/utils/logger.js';
 
 dotenv.config({
-    path: '../../../.env'
+    path:"./.env"
+})
+
+const PORT = process.env.PORT || 5000;
+
+connectDB()
+.then(() => {
+    app.listen(PORT, () => {
+        logger.info(`Server is running on port ${PORT}`);
+    })
+})
+.catch((err) => {
+    logger.error("MONGO db connection failed !!! ", err);
 })
